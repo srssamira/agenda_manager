eb1d14fece203f7402aaeadf7de8b972848c1f97
Samira <srs.samira.silva@gmail.com>
2024-09-23 21:18:28 -0300

    add dtos in controller packpage

--- src/main/java/com/agenda/agenda_manager/controllers/dtos/AgendaDTO.java
+++ src/main/java/com/agenda/agenda_manager/controllers/dtos/AgendaDTO.java
@@ -1,4 +1,52 @@
 package com.agenda.agenda_manager.controllers.dtos;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
 
 public class AgendaDTO {
+    Date startDate;
+    Date endDate;
+    Time startTime;
+    Time endTime;
+    List<EventDTO> events;
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public Time getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Time endTime) {
+        this.endTime = endTime;
+    }
+
+    public List<EventDTO> getEvents() {
+        return events;
+    }
+
+    public void setEvents(List<EventDTO> events) {
+        this.events = events;
+    }
 }
--- src/main/java/com/agenda/agenda_manager/controllers/dtos/EventDTO.java
+++ src/main/java/com/agenda/agenda_manager/controllers/dtos/EventDTO.java
@@ -1,4 +1,51 @@
 package com.agenda.agenda_manager.controllers.dtos;
 
+import java.sql.Time;
+
 public class EventDTO {
+    String id;
+    String eventName;
+    String description;
+    Time startTime;
+    boolean activeEvent;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Time getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Time startTime) {
+        this.startTime = startTime;
+    }
+
+    public boolean isActiveEvent() {
+        return activeEvent;
+    }
+
+    public void setActiveEvent(boolean activeEvent) {
+        this.activeEvent = activeEvent;
+    }
 }
